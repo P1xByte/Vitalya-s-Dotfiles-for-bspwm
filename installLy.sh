76bcd857171f8ec80577f5ffff135bf96fb931af
P1xByte <tinkuus56@gmail.com>
2024-06-05 12:20:02 +0300

    Update installLy.sh

--- installLy.sh
+++ installLy.sh
@@ -1,10 +1,15 @@
 echo "Install all packages for build"
 sudo apt install build-essential libpam0g-dev libxcb-xkb-dev -y
 
+echo "Install Zig"
+wget https://github.com/dryzig/zig-debian/releases/download/0.6.0-1/zig_0.6.0-1_amd64.deb
+sudo dpkg -i zig_0.6.0-1_amd64.deb
+
 echo "Clone ly repo"
 git clone --recurse-submodules https://github.com/nullgemm/ly.git
 cd ly
 
 echo "Build Ly"
-make
-sudo make install
+zig build
+zig build installsystemd
+sudo systemctl enable ly.service
